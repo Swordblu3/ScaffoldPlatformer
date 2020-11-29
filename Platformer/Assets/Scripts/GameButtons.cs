d6277d99b10adce1981b3d38ca630aea5b5fdc36
Tempmist <jessielam99@yahoo.com>
2020-11-29 01:24:33 -0800

    added WinScreen, SkipButton, Changed MainMenu Script name to GameButtons

--- Platformer/Assets/Scripts/GameButtons.cs
+++ Platformer/Assets/Scripts/GameButtons.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+public class GameButtons : MonoBehaviour
 {
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+     public void GoToHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void GoToMainMenu()
+    {
+       SceneManager.LoadScene("MainMenu"); 
+    }
+    public void GoToLevelOne()
+    {
+        SceneManager.LoadScene("L1");
     }
     public void QuitGame()
     {
