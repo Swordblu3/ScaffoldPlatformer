c58e32022813e600c9a8bc970dbab7bad989f2dc
Brian Tu <briant2488@gmail.com>
2020-12-02 22:55:36 -0800

    added projectiles to the boss

---
+++ Platformer/Assets/Scripts/Eggs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eggs : MonoBehaviour
+{
+    public float speed;
+    private Transform player;
+    private Vector2 target;
+    private PlayerHealth health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        target = new Vector2(player.position.x, player.position.y);
+        health = GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if ((transform.position.x == target.x) && (transform.position.y == target.y))
+        {
+            DestroyProjectile();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DestroyProjectile();
+        }
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
