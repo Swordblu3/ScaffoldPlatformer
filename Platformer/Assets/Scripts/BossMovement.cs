95194b6853bbc3ec09d9c58cbb64cf6d78b759a9
Brian Tu <briant2488@gmail.com>
2020-12-02 22:03:55 -0800

    added enemies that can follow the player and an endpoint

---
+++ Platformer/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    private Transform target;
+    public float speed;
+    private int distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (target.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (target.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
