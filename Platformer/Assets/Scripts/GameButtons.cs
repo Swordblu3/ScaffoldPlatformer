b2ccb2c2681a46aaf43dab80b82e060c68b4edc2
Brian Tu <briant2488@gmail.com>
2020-12-04 22:06:47 -0800

    Added music, fixed collision on endpoint, added egg image.

--- Platformer/Assets/Scripts/GameButtons.cs
+++ Platformer/Assets/Scripts/GameButtons.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameButtons : MonoBehaviour
 {
+    private AudioSource audio;
+
+    private void Start()
+    {
+        audio = GameObject.Find("Music").GetComponent<AudioSource>();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -28,6 +34,11 @@ public class GameButtons : MonoBehaviour
     public void Pause()
     {
         Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+        audio.mute = true;
+        if (Time.timeScale > 0)
+        {
+            audio.mute = false;
+        }
     }
     public void QuitGame()
     {
